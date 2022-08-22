a478894cfb8731e9d77e8fdc64e4915199be352a
Juan Pablo <56686083+D-sant@users.noreply.github.com>
2022-08-22 14:45:59 -0400

    Add files via upload

---
+++ php/conexion_be.php
@@ -0,0 +1,11 @@
+<?php
+    $conexion=mysqli_connect("localhost","root","","login_register_db");
+     
+    /*if($conexion){
+          echo'Conectado exitosamenteala Base de Datos';
+     }else{
+          echo'No se ha podido conectarala Base de Datos';
+    }
+    */
+?>
+     
